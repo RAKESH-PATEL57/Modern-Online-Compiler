efd01636f8be0106cb639442ad299b15c962ed94
Rakesh Patel <rakesh246808@gmail.com>
2023-07-09 00:04:16 +0530

    creating API

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ var editor = CodeMirror.fromTextArea(document.getElementById("editor"),{
 })
 var width = window.innerWidth
 editor.setSize(0.7*width,"500")
+
+var option = document.getElementById("inlineFormSelectPref")
+option.addEventListener("change", function(){
+    if(option.value == "Java")
+    {
+        editor.setOption("mode","text/x-java")
+    }
+
+    else if(option.value == "C++")
+    {
+        editor.setOption("mode","text/x-c++")
+    }
+
+    else
+    {
+        editor.setOption("mode","text/x-python")
+    }
+
+})
