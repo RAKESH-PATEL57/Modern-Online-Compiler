83477b189e06aaf7c4142d0ee0f55cc1314ef882
Rakesh Patel <rakesh246808@gmail.com>
2023-07-12 18:45:37 +0530

    styling

--- Api.js
+++ Api.js
@@ -1,3 +1,12 @@
 const express = require("express")
 const app = express()
 const bodyP = require("body-parser")
+
+app.use(bodyP.json())
+app.use("/codemirror-5.65.13", express.static("D:/html,css,javascript projects/Online Compiler/codemirror-5.65.13"))
+
+app.use("/", function (req, res){
+    res.sendFile("D:/html,css,javascript projects/Online Compiler/index.html")
+})
+
+app.listen(8000)
